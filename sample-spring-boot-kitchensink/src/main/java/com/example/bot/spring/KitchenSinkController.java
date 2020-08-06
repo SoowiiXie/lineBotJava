a0c816305bf92ced40e30382f9f20a2226e496c3
Soowii <e64009117@gs.ncku.edu.tw>
2020-08-06 23:46:33 +0800

    這次直接刪掉看看

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -129,39 +129,6 @@ public class KitchenSinkController {
     }
 
     @EventMapping
-    public void handleImageMessageEvent(MessageEvent<ImageMessageContent> event){
-        // You need to install ImageMagick
-    	try {
-	        handleHeavyContent(
-	                event.getReplyToken(),
-	                event.getMessage().getId(),
-	                responseBody -> {
-	                	throw new Exception();
-	                    final ContentProvider provider = event.getMessage().getContentProvider();
-	                    final DownloadedContent jpg;
-	                    final DownloadedContent previewImg;
-	                    if (provider.isExternal()) {
-	                        jpg = new DownloadedContent(null, provider.getOriginalContentUrl());
-	                        previewImg = new DownloadedContent(null, provider.getPreviewImageUrl());
-	                    } else {
-	                        jpg = saveContent("jpg", responseBody);
-	                        previewImg = createTempFile("jpg");
-	                        system(
-	                                "convert",
-	                                "-resize", "240x",
-	                                jpg.path.toString(),
-	                                previewImg.path.toString());
-	                    }
-	                    reply(event.getReplyToken(),
-	                          new ImageMessage(jpg.getUri(), previewImg.getUri()));
-	                }
-	                );
-    	}catch(Exception e) {
-    		
-    	}
-    }
-
-    @EventMapping
     public void handleAudioMessageEvent(MessageEvent<AudioMessageContent> event) throws IOException {
         handleHeavyContent(
                 event.getReplyToken(),
